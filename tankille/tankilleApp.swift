4865ac4347a3aefe74d7ac3bd4e759762a064e32
Leevi Aattola <leevi.aattola@geniem.com>
2023-07-27 10:05:41 +0300

    Initial Commit

---
+++ tankille/tankilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tankilleApp.swift
+//  tankille
+//
+//  Created by Leevi Aattola on 27.7.2023.
+//
+
+import SwiftUI
+
+@main
+struct tankilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
